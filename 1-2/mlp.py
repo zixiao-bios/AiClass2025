ce0059adfc6acb33ee7bf10709f68be03a4d100c
zixiao_bios <zixiao_bios@outlook.com>
2025-01-16 00:13:57 +0800

    修改1-2代码

--- 1-2/mlp.py
+++ 1-2/mlp.py
@@ -26,11 +26,11 @@ class SimpleMLP(nn.Module):
 
 if __name__ == '__main__':
     # 读入数据、转为 tensor、绘制
-    X, Y = utils.read_csv_data('data_2.csv')
+    X, Y = utils.read_csv_data('data_1.csv')
     X = torch.tensor(X, dtype=torch.float32)
     Y = torch.tensor(Y, dtype=torch.float32)
     print(X.shape, Y.shape)
-    # utils.draw_2d_scatter(X, Y)
+    utils.draw_2d_scatter(X, Y)
     
     # 模型、损失函数、优化器
     model = SimpleMLP()
@@ -41,35 +41,27 @@ if __name__ == '__main__':
     dataset = TensorDataset(X, Y)
     data_loader = DataLoader(dataset, batch_size=16, shuffle=True)
 
-    # ===================== 4. 训练模型 =====================
+    # 训练模型
     epochs = 50
     for epoch in range(epochs):
         epoch_loss = 0
         
         for batch_x, batch_y in data_loader:
-            # 使用模型预测输出
+            # 预测输出、计算损失
             predictions = model(batch_x)
-            
-            # 计算损失
             loss = criterion(predictions, batch_y)
             
-            # 梯度清零，防止累积
+            # 计算梯度、更新参数
             optimizer.zero_grad()
-            
-            # 计算梯度
             loss.backward()
-            
-            # 更新参数
             optimizer.step()
             
             # 累积损失
             epoch_loss += loss.item()
 
-        # 打印训练过程中的损失值
+        # 打印本轮的损失值
         print(f'Epoch {epoch}, Loss: {epoch_loss / len(data_loader)}')
 
-
-    # ===================== 5. 查看结果 =====================
     # 使用训练好的模型预测
     predicted = model(X)
     utils.draw_2d_scatter(X, Y, predicted.detach().numpy())
